e4d5bdf61f20c3d33e70510f4a26353626d45711
Huffaker.lily <Huffaker.lily@SEMS-259-18.SEMS259.LAB>
2019-01-16 16:30:42 -0800

    YOu stressed over one project for forever

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,4 +1,27 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class TeaParty {
-
+	
+	 public String welcome(String name, boolean isWoman, boolean isKnighted) {
+		 String greeting = "";
+		 if(isKnighted == false) {
+			 if(isWoman == true) {
+			 greeting = ("Ms. ");
+		 }
+		 else if(isWoman == false) {
+			 greeting = ("Mr. ");
+		 }
+		 }
+		 else if(isKnighted == true){
+			 if(isWoman == true) {
+				 greeting = ("Lady ");
+			 }
+			 if(isWoman == false) {
+				 greeting = ("Sir ");
+			 }
+		 }
+		return "Hello "+greeting+name;
+     }
+	 
 }
